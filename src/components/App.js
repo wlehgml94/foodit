e431883ca0725a0ebc12eb36cfaf46a8d53c05e3
wlehgml94 <wlehgml94@naver.com>
2023-11-10 16:15:34 +0900

    231110 : 삭제 버튼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import FoodList from './FoodList';
-import items from '../mock.json';
+import morkItems from '../mock.json';
 import { useState } from "react";
 
 
 function App() {
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   const [order, setOrder] = useState('createdAt');
+  const [items, setItems] = useState(morkItems);
   const sortedItems = items.sort((a,b) => b[order] - a[order]);
   const handleNewestClick = () => {
     setOrder("createdAt");
@@ -17,7 +22,7 @@ function App() {
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handlecaloClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -5,25 +5,30 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function FoodListItem({ item }) {
+function FoodListItem({ item , onDelete }) {
   const { imgUrl, title, calorie, content, createdAt } = item;
 
+  const handleDeleteClick = () => onDelete(item.id);
+
   return (
-    <div>
+    <div className='FoodListItem'>
       <img src={imgUrl} alt={title} />
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
       <div>{formatDate(createdAt)}</div>
+      <button onClick={handleDeleteClick}>삭제</button>
     </div>
   );
 }
 
-function FoodList({ items }) {
+function FoodList({ items, onDelete }) {
   return (
-  <ul>
+  <ul className='FoodList'>
     {items.map((item) => (
-     <li><FoodListItem item={item} /></li>
+     <li>
+      <FoodListItem item={item} onDelete={onDelete} />
+      </li>
     ))}
     
   </ul>
